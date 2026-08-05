9c931bae6d752abdd2d9db5657079f601aed82dd
Curtis Lewis <lewis.curtis.t@gmail.com>
2026-08-05 15:22:24 -0600

    added level switching

--- game.gd
+++ game.gd
@@ -1,11 +1,16 @@
 extends Node
 
+@onready var sandbox: Node2D = $Sandbox
+@onready var slot_machine_view: Node2D = $SlotMachineView
+@onready var slot_machine_camera: Camera2D = $SlotMachineView/Node2D/Camera2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Game scene is ready.")
-	pass # Replace with function body.
+	slot_machine_view.visible = false
+	GameEvents.slot_machine_opened.connect(_on_slot_machine_opened)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_slot_machine_opened() -> void:
+	sandbox.visible = false
+	sandbox.process_mode = Node.PROCESS_MODE_DISABLED
+	slot_machine_view.visible = true
+	slot_machine_camera.make_current()
--- games/slot_machine.gd
+++ games/slot_machine.gd
@@ -5,7 +5,7 @@ var player_in_range: bool = false
 
 func _process(_delta: float) -> void:
 	if player_in_range and Input.is_action_just_pressed('interact'):
-		print('We are interacting!')
+		GameEvents.slot_machine_opened.emit()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group('player'):
