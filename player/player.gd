49be9f230cd95d6200db25e657352af1627538b4
Curtis Lewis <lewis.curtis.t@gmail.com>
2026-08-05 13:28:13 -0600

    added movement and base sandbox level

---
+++ player/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED: float = 50.0
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	handle_input()
+
+func handle_input() -> void:
+	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+	if direction == Vector2.RIGHT:
+		animated_sprite.flip_h = false
+	if direction == Vector2.LEFT:
+		animated_sprite.flip_h = true
+
+	if direction != Vector2.ZERO:
+		animated_sprite.play('move_right')	
+	else:
+		animated_sprite.play('idle')
+
+	velocity = direction * SPEED
+	move_and_slide()
